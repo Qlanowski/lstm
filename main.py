5c7a8baaa3b6ef3647953431b1eb9ec918c9a09a
ulanowskik <ulanowskik@p30312w.MINI>
2020-06-01 22:09:03 +0200

    loader file

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main(config_paths):
     for config_path in config_paths:
         with open(config_path, 'r') as file:
             json_config = file.read()
+
+        
         #     config = tester.TestConfig.from_json(json_config)
         # print(f"Test {config.test_name} started")
         # #network = tester.perform_test(config)
