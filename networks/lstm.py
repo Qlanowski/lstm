f3665b611a5e4a55d0eb772fa5357ca7008c2696
ulanowskik <ulanowskik@p30312w.MINI>
2020-06-02 08:58:58 +0200

    simple lstm

---
+++ networks/lstm.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+class LSTM_fixed_len(torch.nn.Module) :
+    def __init__(self, embedding_dim, hidden_dim, classes):
+        super().__init__()
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.linear = nn.Linear(hidden_dim, classes)
+        self.dropout = nn.Dropout(0.2)
+        
+    def forward(self, x, l):
+        x = self.dropout(x)
+        lstm_out, (ht, ct) = self.lstm(x)
+        return self.linear(ht[-1])
