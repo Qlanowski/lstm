423058f1d4f36f105db86d5dbc1f4e6b3bef9478
Jakub Waszkiewicz <qbazilla@hotmail.com>
2020-06-14 15:30:03 +0200

    fix adding silence data script

--- cleaning/silence.py
+++ cleaning/silence.py
@@ -1,20 +1,24 @@
 from shutil import copyfile
 import os
 
-dst_dir = './data/train/audio/silence/'
-if not os.path.exists(dst_dir):
-    os.makedirs(dst_dir)
+dst_dir = "./data/train/audio/silence"
+os.makedirs(dst_dir, exist_ok=True)
 
-f = open("./cleaning/bad_files.txt", "r")
-files = f.read().split('\n')
+with open("./cleaning/bad_files.txt", "r") as f:
+    files = f.read().split('\n')
 
 cnt = 0
 for file in files:
-    src = f"./full_data/train/audio/{file}"
-    file_name = file.split("/")[-1]
-    dst = f"{dst_dir}{file_name}"
+    src = os.path.join("./full_data/train/audio", file)
+    file_name = "{}_{}".format(
+        os.path.dirname(file),
+        os.path.basename(file)
+    )
+    dst = os.path.join(dst_dir, file_name)
     if os.path.exists(src) and not os.path.exists(dst):
         copyfile(src, dst)
         cnt += 1
+    else:
+        print("NOT COPIED", src, dst)
 
 print(f"copied {cnt}/{len(files)}")
